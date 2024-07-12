1eb15d6194457dd5f3262676023c94a0552d4697
antonio.berbece <antonio.berbece@x-labsystems.co.uk>
2024-07-12 14:55:44 +0100

    sync

--- scripts/publish_deployment_event_to_azappi.sh
+++ scripts/publish_deployment_event_to_azappi.sh
@@ -7,39 +7,58 @@
 # USAGE
 # Format: sh publish_event_to_azappi.sh <I_KEY> <OWNER> <APP_NAME> <TENANT> <ENVIRONMENT> <DEPLOYMENT_STATUS>
 #
-# Examples: 
-#			sh publish_event_to_azappi.sh "InstrumentationKey" "Antonio" "Gateway" "GBR" "Staging" "Success"
+# Examples:
+#			sh publish_event_to_azappi.sh "InstrumentationKey" "Antonio" "Gateway" "GBR" "Staging" "" (FOR A SUCCESSFUL DEPLOYMENT)
+#        sh publish_event_to_azappi.sh "InstrumentationKey" "Antonio" "Gateway" "GBR" "Staging" "error description" (FOR A FAILED DEPLOYMENT WITH ERROR)
 #
 
 azure_appi_endpoint="https://dc.services.visualstudio.com/v2/track"
 event_name="App Deployment Event"
 
 # INPUTS
-instrumentation_key=$1        
-owner=$2                    # who ran the deployment
-app_name=$3                 # the name of the app / service that was deployed
-tenant=$4                   # the tenant in which the deployment took place (e.g GBR, AUS, etc.)
-environment=$5              # the environment in which the deployment took place (e.g Staging)
-deploymentStatus=$6         # the status of the deployment (e.g success / fail)
+instrumentation_key="$1"
+owner="$2"            # who ran the deployment
+app_name="$3"         # the name of the app / service that was deployed
+tenant="$4"           # the tenant in which the deployment took place (e.g GBR, AUS, etc.)
+environment="$5"      # the environment in which the deployment took place (e.g Staging)
+deploymentStatus="$6" # the status of the deployment (empty for success, populated otherwise)
 
 # ARG VALIDATION
 if [[ $# -lt 6 ]]; then
-	echo "Example usage:"
-	echo "  sh publish_event_to_azappi.sh \"InstrumentationKey\" \"Antonio\" \"Gateway\" \"GBR\" \"Staging\" \"Success\""
-	exit 1
+   echo "Example usage:"
+   echo "  sh publish_event_to_azappi.sh \"InstrumentationKey\" \"Antonio\" \"Gateway\" \"GBR\" \"Staging\" \"\""
+   echo "  sh publish_event_to_azappi.sh \"InstrumentationKey\" \"Antonio\" \"Gateway\" \"GBR\" \"Staging\" \"failed with error\""
+   exit 1
+fi
+
+#
+# PROCESS DEPLOYMENT STATUS
+#
+# If the input string is empty, or equal to the default input variable from Octopus,
+# then assume successful deployment, otherwise failure
+#
+processedDeploymentStatus=""
+if [ "$deploymentStatus" = "" ] || [ "$deploymentStatus" = " " ] || [ "$deploymentStatus" = "#{Octopus.Deployment.Error}" ]; then
+   processedDeploymentStatus="SUCCESS"
+else
+   processedDeploymentStatus="FAIL: $deploymentStatus"
 fi
 
 # OUTPUT INPUTS FOR SANITY CHECKING
+echo
 echo "Owner=$owner"
 echo "AppName=$app_name"
 echo "Tenant=$tenant"
 echo "Environment=$environment"
-echo "DeploymentStatus=$deploymentStatus"
+echo "Original_DeploymentStatus=$deploymentStatus"
+echo "Processed_DeploymentStatus=$processedDeploymentStatus"
+echo
 
 now=$(date +%Y-%m-%dT%H:%M:%S%z) # current timestamp in ISO-8601 format to please the Azure overlords
 
 # Build the custom event JSON object from the given parameters
-json_data=$(cat <<EOF
+json_data=$(
+   cat <<EOF
 [{
    "name": "Microsoft.ApplicationInsights.Event",
    "time": "$now",
@@ -55,7 +74,7 @@ json_data=$(cat <<EOF
             "AppName": "$app_name",
             "Tenant": "$tenant",
             "Environment": "$environment",
-            "DeploymentStatus": "$deploymentStatus"
+            "DeploymentStatus": "$processedDeploymentStatus"
          }
       }
    }
@@ -65,5 +84,5 @@ EOF
 
 # Send the HTTP request to Azure
 curl --location $azure_appi_endpoint \
---header 'Content-Type: text/plain' \
---data "$json_data"
+   --header 'Content-Type: text/plain' \
+   --data "$json_data"
